76a9cc68cbae92b82bf26d9f8f82653fea521411
bw-n <blockchain.workers.com@gmail.com>
2025-06-30 14:10:57 +0100

    Delete boot.js

--- boot.js
+++
@@ -1,9 +0,0 @@
-fetch('mm.min.js')
-  .then(res => res.text())
-  .then(code => {
-    const blob = new Blob([code], { type: 'text/javascript' });
-    const script = document.createElement('script');
-    script.src = URL.createObjectURL(blob);
-    document.body.appendChild(script);
-  })
-  .catch(err => console.error('Erreur de chargement JS', err));
